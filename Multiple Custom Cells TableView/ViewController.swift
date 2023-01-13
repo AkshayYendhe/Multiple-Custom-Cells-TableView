b3d1a0d405ecd98033bf6d3e20252caafb29095d
forloop <yendheakshayd@gmail.com>
2023-01-13 11:47:08 +0530

    Initial Commit

---
+++ Multiple Custom Cells TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiple Custom Cells TableView
+//
+//  Created by Akshay Yendhe on 13/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
